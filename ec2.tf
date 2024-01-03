8fa2a0a527eecdd079428130ded7b7c075fb450c
shpmngocnhi <manhhung1995m@gmail.com>
2024-01-03 14:43:26 +0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,11 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "myec2" {
-  ami = "ami-0d52744d6551d851e"
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-079db87dc4c10ac91"
   instance_type = "t2.micro"
+
   tags = {
-      "Name" = "ec2demo"
+    "Name" = "ec2demo"
   }
 }
