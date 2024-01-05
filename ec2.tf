b9956184465c1787af2e8bd2b2cbc35cd5f61475
shpmngocnhi <manhhung1995m@gmail.com>
2024-01-05 15:03:03 +0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2demo" {
   ami           = "ami-0dfa284c9d7b2adad"
   instance_type = "t2.micro"
 
-  tags = {
-    "Name" = "ec2demo"
-  }
+  #tags = {
+  #  "Name" = "ec2demo"
+  #}
 }
